8f564e6b2b20e117c1a33e73ce0e44720615df9c
DevAccount <devaccount@DevAccounts-Mac-mini.local>
2026-08-25 07:27:37 -0700

    Set booth number position from precise pixel analysis of frame image

--- script.js
+++ script.js
@@ -122,8 +122,8 @@
             const fontSize = currentStyle === 'banner' ? "22px" : "34px";
             const fontName = "'NeueHaasGrotesk', 'Inter', sans-serif";
             
-            const textCenterX = currentStyle === 'banner' ? 551 : 838;
-            const textBaselineY = currentStyle === 'banner' ? 80 : 155;
+            const textCenterX = currentStyle === 'banner' ? 551 : 854;
+            const textBaselineY = currentStyle === 'banner' ? 80 : 184;
 
             ctx.fillStyle = "#023149"; // Dark blue text color
             ctx.font = `700 ${fontSize} ${fontName}`;
